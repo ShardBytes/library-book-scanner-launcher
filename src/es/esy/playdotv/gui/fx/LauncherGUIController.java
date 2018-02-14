96dfcb0966fcd2d313b2d1c0721fb4c51564ac45
Plajdo <31418257+Plajdo@users.noreply.github.com>
2018-02-14 17:22:15 +0100

    Slight edit

--- src/es/esy/playdotv/gui/fx/LauncherGUIController.java
+++ src/es/esy/playdotv/gui/fx/LauncherGUIController.java
@@ -22,10 +22,8 @@ public class LauncherGUIController{
 		    jvm_location = System.getProperties().getProperty("java.home") + File.separator + "bin" + File.separator + "java";
 		}
 		
-		File lbsJar = new File("runtime\\lbs_1.1.0.jar");
-		
 		try {
-			Runtime.getRuntime().exec(jvm_location + " -jar " + lbsJar.toString());	//Doesn't work for some reason - LBS won't open
+			Runtime.getRuntime().exec(jvm_location + " -jar " + "resources/lbs.jar");
 		} catch (IOException e1) {
 			e1.printStackTrace();
 		}
