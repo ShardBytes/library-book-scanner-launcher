9233f1708f5e8c8f4e40707912708b0d6d9f67dd
Plajdo <31418257+Plajdo@users.noreply.github.com>
2018-02-24 13:36:51 +0100

    Windows ANSI terminal bug fix

--- src/es/esy/playdotv/gui/terminal/TermUtils.java
+++ src/es/esy/playdotv/gui/terminal/TermUtils.java
@@ -10,19 +10,32 @@ public class TermUtils{
 	private static ColoredPrinter cp;
 	
 	public static void init(){
-		//cp = new ColoredPrinter.Builder(1, false).foreground(FColor.WHITE).background(BColor.BLACK).build();
+		if(System.getProperty("os.name").startsWith("Win")){
+			//Do nothing
+		}else{
+			cp = new ColoredPrinter.Builder(1, false).foreground(FColor.WHITE).background(BColor.BLACK).build();
+		}
+		
 	}
 	
 	public static void println(String text){
-		//cp.print(cp.getDateFormatted(), Attribute.NONE, FColor.CYAN, BColor.BLACK);
-		//cp.println(" " + text);
-		System.out.println(text);
+		if(System.getProperty("os.name").startsWith("Win")){
+			System.out.println(text);
+		}else{
+			cp.print(cp.getDateFormatted(), Attribute.NONE, FColor.CYAN, BColor.BLACK);
+			cp.println(" " + text);
+		}
+		
 	}
 	
 	public static void printerr(String text){
-		//cp.print(cp.getDateFormatted(), Attribute.NONE, FColor.RED, BColor.BLACK);
-		//cp.println(" " + text, Attribute.NONE, FColor.RED, BColor.BLACK);
-		System.err.println(text);
+		if(System.getProperty("os.name").startsWith("Win")){
+			System.err.println(text);
+		}else{
+			cp.print(cp.getDateFormatted(), Attribute.NONE, FColor.RED, BColor.BLACK);
+			cp.println(" " + text, Attribute.NONE, FColor.RED, BColor.BLACK);
+		}
+		
 	}
 	
 }
