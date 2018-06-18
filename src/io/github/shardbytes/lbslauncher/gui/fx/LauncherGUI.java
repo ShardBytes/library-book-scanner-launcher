0f10f7d4c02b837ceae7272630ea3ea4645aec49
Plajdo <31418257+Plajdo@users.noreply.github.com>
2018-06-18 15:23:15 +0200

    use task

--- src/io/github/shardbytes/lbslauncher/gui/fx/LauncherGUI.java
+++ src/io/github/shardbytes/lbslauncher/gui/fx/LauncherGUI.java
@@ -1,13 +1,19 @@
 package io.github.shardbytes.lbslauncher.gui.fx;
 
+import com.sun.xml.internal.xsom.impl.Ref;
 import io.github.shardbytes.lbslauncher.gui.terminal.TermUtils;
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.concurrent.Task;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.File;
+import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
 import java.nio.file.FileAlreadyExistsException;
@@ -26,6 +32,8 @@ public class LauncherGUI extends Application{
 
 	@Override
 	public void start(Stage primaryStage) throws Exception{
+		initLoading();
+		
 		FXMLLoader loader = new FXMLLoader(getClass().getResource("MainWindow.fxml"));
 		root = loader.load();
 		LauncherGUIController controllerInstance = loader.getController();
@@ -56,6 +64,10 @@ public class LauncherGUI extends Application{
 	}
 	
 	public static void main(String[] args){
+		LauncherGUI.launch();
+	}
+	
+	public void initLoading(){
 		/*
 		 * Init coloured printer
 		 */
@@ -63,42 +75,74 @@ public class LauncherGUI extends Application{
 		if(System.console() == null){
 			System.setProperty("jansi.passthrough", "true");
 		}
-		
 		try{
-			Files.createDirectories(Paths.get("resources"));
-			Files.createDirectories(Paths.get("data"));
-			File splashFile = new File("resources" + File.separator + "splash.png");
-			File zoznamFile = new File("resources" + File.separator + "zoznam.xls");
-			File booksFile = new File("resources" + File.separator + "books.xls");
-			File personsFile = new File("resources" + File.separator + "persons.xls");
-			if(!splashFile.exists() || splashFile.isDirectory()
-					|| !zoznamFile.exists() || zoznamFile.isDirectory()
-					|| !booksFile.exists() || booksFile.isDirectory()
-					|| !personsFile.exists() || personsFile.isDirectory()){
-				URL splash = new URL("https://raw.githubusercontent.com/ShardBytes/library-book-scanner-launcher/master/splash.png");
-				URL zoznam = new URL("https://github.com/ShardBytes/library-book-scanner/raw/master/zoznam.xls");
-				URL books = new URL("https://github.com/ShardBytes/library-book-scanner/raw/master/books.xls");
-				URL persons = new URL("https://github.com/ShardBytes/library-book-scanner/raw/master/persons.xls");
-				TermUtils.println("Downloading resources...");
-				InputStream splash_in = splash.openStream();
-				InputStream zoznam_in = zoznam.openStream();
-				InputStream books_in = books.openStream();
-				InputStream persons_in = persons.openStream();
-				Files.copy(splash_in, Paths.get("resources" + File.separator + "splash.png"), StandardCopyOption.REPLACE_EXISTING);
-				Files.copy(zoznam_in, Paths.get("resources" + File.separator + "zoznam.xls"), StandardCopyOption.REPLACE_EXISTING);
-				Files.copy(books_in, Paths.get("resources" + File.separator + "books.xls"), StandardCopyOption.REPLACE_EXISTING);
-				Files.copy(persons_in, Paths.get("resources" + File.separator + "persons.xls"), StandardCopyOption.REPLACE_EXISTING);
-			}
+			final Stage dialog = new Stage(StageStyle.TRANSPARENT);
+			final FXMLLoader loader = new FXMLLoader(getClass().getResource("UpdatePopup.fxml"));
+			final Parent root = loader.load();
+			final Scene scene = new Scene(root);
+			final UpdatePopupController controller = loader.getController();
 			
+			dialog.initModality(Modality.APPLICATION_MODAL);
+			dialog.setAlwaysOnTop(true);
+			dialog.setResizable(false);
+			dialog.setScene(scene);
 			
+			Task<Void> downloadTask = new Task<Void>(){
+				@Override
+				protected Void call(){
+					try{
+						Files.createDirectories(Paths.get("resources"));
+						Files.createDirectories(Paths.get("data"));
+						File splashFile = new File("resources" + File.separator + "splash.png");
+						File zoznamFile = new File("resources" + File.separator + "zoznam.xls");
+						File booksFile = new File("resources" + File.separator + "books.xls");
+						File personsFile = new File("resources" + File.separator + "persons.xls");
+						if(!splashFile.exists() || splashFile.isDirectory()
+								|| !zoznamFile.exists() || zoznamFile.isDirectory()
+								|| !booksFile.exists() || booksFile.isDirectory()
+								|| !personsFile.exists() || personsFile.isDirectory()){
+							URL splash = new URL("https://raw.githubusercontent.com/ShardBytes/library-book-scanner-launcher/master/splash.png");
+							URL zoznam = new URL("https://github.com/ShardBytes/library-book-scanner/raw/master/zoznam.xls");
+							URL books = new URL("https://github.com/ShardBytes/library-book-scanner/raw/master/books.xls");
+							URL persons = new URL("https://github.com/ShardBytes/library-book-scanner/raw/master/persons.xls");
+							TermUtils.println("Downloading resources...");
+							InputStream splash_in = splash.openStream();
+							InputStream zoznam_in = zoznam.openStream();
+							InputStream books_in = books.openStream();
+							InputStream persons_in = persons.openStream();
+							Files.copy(splash_in, Paths.get("resources" + File.separator + "splash.png"), StandardCopyOption.REPLACE_EXISTING);
+							controller.setProgress(0, 100, 25);
+							Files.copy(zoznam_in, Paths.get("resources" + File.separator + "zoznam.xls"), StandardCopyOption.REPLACE_EXISTING);
+							controller.setProgress(0, 100, 50);
+							Files.copy(books_in, Paths.get("resources" + File.separator + "books.xls"), StandardCopyOption.REPLACE_EXISTING);
+							controller.setProgress(0, 100, 75);
+							Files.copy(persons_in, Paths.get("resources" + File.separator + "persons.xls"), StandardCopyOption.REPLACE_EXISTING);
+							controller.setProgress(0, 100, 100);
+							
+						}
+						
+					}catch(FileAlreadyExistsException ignored){
+					}catch(Exception e){
+						TermUtils.printerr("Something happened");
+						TermUtils.printerr(e.getMessage());
+					}finally{
+						TermUtils.println("Resources ready, launching");
+						dialog.close();
+					}
+					return null;
+					
+				}
+				
+			};
 			
-		}catch(FileAlreadyExistsException ignored){
-		}catch(Exception e){
-			TermUtils.printerr("Something happened");
+			downloadTask.setOnRunning((event) -> dialog.show());
+			downloadTask.setOnSucceeded((event -> dialog.close()));
+			downloadTask.setOnFailed((event -> Platform.exit()));
+			
+			new Thread(downloadTask).start();
+			
+		}catch(IOException e){
 			TermUtils.printerr(e.getMessage());
-		}finally{
-			TermUtils.println("Resources ready, launching");
-			LauncherGUI.launch(args);
 		}
 
 	}
